443fcd219edb305f4cc2544364e708dd80bc351b
Karina Miernik <karina.miernik@gmail.com>
2021-11-19 22:28:17 +0100

    updated config

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import App from './App'
 import './index.css'
 import { initializeApp } from "@firebase/app";
 const firebaseConfig = {
-  apiKey: "AIzaSyD21woL6mg7-j-yLJhJ_6M-0AlVL8s45Vs",
-  authDomain: "herbmed-f48db.firebaseapp.com",
-  databaseURL: "https://herbmed-f48db-default-rtdb.europe-west1.firebasedatabase.app/",
-  projectId: "herbmed-f48db",
-  storageBucket: "herbmed-f48db.appspot.com",
-  messagingSenderId: "798029530520",
-  appId: "1:798029530520:web:efa97a6e772119e1dcd7ed",
-  measurementId: "G-F09RL23GRN"
+  apiKey: "AIzaSyDt0pwyDI5XmTlFcKyNqLQuM-wKIdUqJuI",
+  authDomain: "herbmed-app-ccdeb.firebaseapp.com",
+  databaseURL: "https://herbmed-app-ccdeb-default-rtdb.firebaseio.com/",
+  projectId: "herbmed-app-ccdeb",
+  storageBucket: "herbmed-app-ccdeb.appspot.com",
+  messagingSenderId: "536625217059",
+  appId: "1:536625217059:web:e9d3ccf9d853ee8498fb96",
+  measurementId: "G-CNLDSPNB4V"
 };
 
 // Initialize Firebase
